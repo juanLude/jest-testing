ac5d63456b053f1b32b8e2947e3671985d609f41
Juan Ludevid <jmludevid@hotmail.com>
2023-12-27 17:36:00 +1100

    first commit

---
+++ src/UserList.js
@@ -0,0 +1,21 @@
+export default function UserList({ users }) {
+  const rederedUsers = users.map((user) => {
+    return (
+      <tr key={user.name}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+      </tr>
+    );
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>{rederedUsers}</tbody>
+    </table>
+  );
+}
